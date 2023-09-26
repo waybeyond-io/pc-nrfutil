11eb26bd215e9f3a16ecfc45da8e3da6e6652448
halfn0rmal <adam@grantelectronics.co.nz>
2023-09-26 16:12:36 +1300

    Hacking an updater

--- README.md
+++ README.md
@@ -1,35 +1,3 @@
 # nRF Util
 
-[![No Maintenance Intended](http://unmaintained.tech/badge.svg)](http://unmaintained.tech/)
-[![Latest version](https://img.shields.io/pypi/v/nrfutil.svg)](https://pypi.python.org/pypi/nrfutil)
-[![License](https://img.shields.io/pypi/l/nrfutil.svg)](https://pypi.python.org/pypi/nrfutil)
-
-## DEPRECATION NOTICE
-This tool is deprecated. The feature set in this tool is available through a new tool with the same name.
-If you need the feature set in this version, please install the new nrfutil from the [official page](https://www.nordicsemi.com/Products/Development-tools/nrf-util).
-
-
-## Intro
-nRF Util is a Python package and command-line utility that supports Device Firmware Updates (DFU) and cryptographic functionality.
-
-![screenshot](screenshot.gif)
-
-## Documentation
-
-See the [InfoCenter](https://infocenter.nordicsemi.com/topic/ug_nrfutil/UG/nrfutil/nrfutil_intro.html) pages for information on how to install and use nRF Util.
-
-## Feedback
-
-Please report issues on the [DevZone](https://devzone.nordicsemi.com) portal.
-
-## Contributing
-
-Feel free to propose changes by creating a pull request.
-
-If you plan to make any non-trivial changes, please start out small and ask seek an agreement before putting too much work in it. A pull request can be declined if it does not fit well within the current product roadmap.
-
-In order to accept your pull request, we need you to sign our Contributor License Agreement (CLA). You will see instructions for doing this after having submitted your first pull request.
-
-## License
-
-See the [LICENSE](LICENSE) file for details.
+## dongle update app
---
+++ update.py
@@ -0,0 +1,80 @@
+import os
+import sys
+import click
+import time
+import logging
+import re
+import serial
+
+from nordicsemi.dfu.bl_dfu_sett import BLDFUSettings
+from nordicsemi.dfu.dfu import Dfu
+from nordicsemi.dfu.dfu_transport import DfuEvent, TRANSPORT_LOGGING_LEVEL
+from nordicsemi.dfu.dfu_transport_serial import DfuTransportSerial
+from nordicsemi.dfu.package import Package
+from nordicsemi import version as nrfutil_version
+from nordicsemi.dfu.signing import Signing
+from nordicsemi.zigbee.prod_config import ProductionConfig, ProductionConfigWrongException, ProductionConfigTooLargeException
+from pc_ble_driver_py.exceptions import NordicSemiException
+from nordicsemi.lister.device_lister import DeviceLister
+import spinel.util as util
+
+logger = logging.getLogger(__name__)
+
+global_bar = None
+def update_progress(progress=0):
+    if global_bar:
+        global_bar.update(progress)
+
+def do_serial(package, port, connect_delay, flow_control, packet_receipt_notification, baud_rate, serial_number, ping,
+              timeout):
+
+    if flow_control is None:
+        flow_control = DfuTransportSerial.DEFAULT_FLOW_CONTROL
+    if packet_receipt_notification is None:
+        packet_receipt_notification = DfuTransportSerial.DEFAULT_PRN
+    if baud_rate is None:
+        baud_rate = DfuTransportSerial.DEFAULT_BAUD_RATE
+    if ping is None:
+        ping = False
+    if port is None:
+        device_lister = DeviceLister()
+        device = device_lister.get_device(serial_number=serial_number)
+        if device is None:
+            raise NordicSemiException("A device with serial number %s is not connected." % serial_number)
+        port = device.get_first_available_com_port()
+        logger.info("Resolved serial number {} to port {}".format(serial_number, port))
+
+    if timeout is None:
+        timeout = DfuTransportSerial.DEFAULT_TIMEOUT
+
+    logger.info("Using board at serial port: {}".format(port))
+    serial_backend = DfuTransportSerial(com_port=str(port), baud_rate=baud_rate,
+                                        flow_control=flow_control, prn=packet_receipt_notification, do_ping=ping,
+                                        timeout=timeout)
+    serial_backend.register_events_callback(DfuEvent.PROGRESS_EVENT, update_progress)
+    dfu = Dfu(zip_file_path = package, dfu_transport = serial_backend, connect_delay = connect_delay)
+
+    if logger.getEffectiveLevel() > logging.INFO:
+        with click.progressbar(length=dfu.dfu_get_total_size()) as bar:
+            global global_bar
+            global_bar = bar
+            dfu.dfu_send_images()
+    else:
+        dfu.dfu_send_images()
+
+    click.echo("Device programmed.")
+
+def usb_serial(package, port, connect_delay, flow_control, packet_receipt_notification, baud_rate, serial_number,
+               timeout):
+    """Perform a Device Firmware Update on a device with a bootloader that supports USB serial DFU."""
+    do_serial(package, port, connect_delay, flow_control, packet_receipt_notification, baud_rate, serial_number, False,
+              timeout)
+
+ser = serial.Serial('/dev/ttyACM0')  # open serial port
+print(ser.name)         # check which port was really used
+ser.write(b'AT ENTERBL\r\n')     # write a string
+ser.close()             # close port
+
+zip_file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'Folium_Dongle-2-6-2-OTA-Image.zip')
+
+usb_serial(zip_file_path, '/dev/ttyACM0', None, None, None, None, None, None)
